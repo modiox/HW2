19aefe6d504bda22671bd6c33209ecdcd2cb7625
Modi <moemuze@gmail.com>
2022-04-20 21:25:47 +0300

    Modified to he4

--- HW2.php
+++ HW2.php
@@ -1,3 +1,21 @@
+<?php
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "";
+ $db = "Tasks";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,11 +27,25 @@
 
 
 <h1 class="title1"> To Do App</h1>
+<br>
 <div class="firstDiv"> 
 <div id="myDIV" class="secondDiv">
   <input type="text" id="myInput" placeholder="Enter your to-do item :)">
   <span onclick="addTask()" class="addBtn">Add</span>
 
+</div>
+<div> 
+ <ul id="myUL">
+  </ul>
+</div>
+</div>
+
+<!-- Second Div -->
+<br> 
+<h1 class="title1">From the Database </h1>
+<br> 
+<div class="firstDiv"> 
+<div id="myDIV" class="secondDiv" class="id">
 </div>
 <div> 
  <ul id="myUL">
